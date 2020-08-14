c56a9fc2fde93538b37191ebb6a2a5c6b479b947
Prashant Gautam <prashant.gtm1001@gmail.com>
2020-08-15 00:27:34 +0530

    Project setup
    Initial project added

---
+++ ExploreCanada/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExploreCanada
+//
+//  Created by Prashant on 15/08/20.
+//  Copyright © 2020 Prashant Gautam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
